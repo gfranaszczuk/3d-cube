17f822f7fd678635ed047f44dd8c26789bef4dcb
gfranaszczuk <gfranaszczuk@gmail.com>
2018-03-04 23:35:45 +0100

    First Commit

---
+++ linki.js
@@ -0,0 +1,160 @@
+
+$('#link-1sc2').click(function() {
+    $('#kostka').removeClass().addClass("turn1sc2");
+
+});
+
+$('#link-1sc3').click(function() {
+    $('#kostka').removeClass().addClass("turn1sc3");
+
+});
+
+$('#link-1sc4').click(function() {
+    $('#kostka').removeClass().addClass("turn1sc4");
+
+});
+
+$('#link-1sc5').click(function() {
+    $('#kostka').removeClass().addClass("turn1sc5");
+
+});
+
+$('#link-1sc6').click(function() {
+    $('#kostka').removeClass().addClass("turn1sc6");
+
+});
+
+// SCIANA 2
+
+$('#link-2sc1').click(function() {
+    $('#kostka').removeClass().addClass("turn2sc1");
+
+});
+
+$('#link-2sc3').click(function() {
+    $('#kostka').removeClass().addClass("turn2sc3");
+
+});
+
+$('#link-2sc4').click(function() {
+    $('#kostka').removeClass().addClass("turn2sc4");
+
+});
+
+$('#link-2sc5').click(function() {
+    $('#kostka').removeClass().addClass("turn2sc5");
+
+});
+
+$('#link-2sc6').click(function() {
+    $('#kostka').removeClass().addClass("turn2sc6");
+
+});
+
+// SCIANA 3
+
+$('#link-3sc1').click(function() {
+    $('#kostka').removeClass().addClass("turn3sc1");
+
+});
+
+$('#link-3sc2').click(function() {
+    $('#kostka').removeClass().addClass("turn3sc2");
+
+});
+
+$('#link-3sc4').click(function() {
+    $('#kostka').removeClass().addClass("turn3sc4");
+
+});
+
+$('#link-3sc5').click(function() {
+    $('#kostka').removeClass().addClass("turn3sc5");
+
+});
+
+$('#link-3sc6').click(function() {
+    $('#kostka').removeClass().addClass("turn3sc6");
+
+});
+
+// SCIANA 4
+
+$('#link-4sc1').click(function() {
+    $('#kostka').removeClass().addClass("turn4sc1");
+
+});
+
+$('#link-4sc2').click(function() {
+    $('#kostka').removeClass().addClass("turn4sc2");
+
+});
+
+$('#link-4sc3').click(function() {
+    $('#kostka').removeClass().addClass("turn4sc3");
+
+});
+
+$('#link-4sc5').click(function() {
+    $('#kostka').removeClass().addClass("turn4sc5");
+
+});
+
+$('#link-4sc6').click(function() {
+    $('#kostka').removeClass().addClass("turn4sc6");
+
+});
+
+// SCIANA 5
+
+$('#link-5sc1').click(function() {
+    $('#kostka').removeClass().addClass("turn5sc1");
+
+});
+
+$('#link-5sc2').click(function() {
+    $('#kostka').removeClass().addClass("turn5sc2");
+
+});
+
+$('#link-5sc3').click(function() {
+    $('#kostka').removeClass().addClass("turn5sc3");
+
+});
+
+$('#link-5sc4').click(function() {
+    $('#kostka').removeClass().addClass("turn5sc4");
+
+});
+
+$('#link-5sc6').click(function() {
+    $('#kostka').removeClass().addClass("turn5sc6");
+
+});
+
+// SCIANA 6
+
+$('#link-6sc1').click(function() {
+    $('#kostka').removeClass().addClass("turn6sc1");
+
+});
+
+$('#link-6sc2').click(function() {
+    $('#kostka').removeClass().addClass("turn6sc2");
+
+});
+
+$('#link-6sc3').click(function() {
+    $('#kostka').removeClass().addClass("turn6sc3");
+
+});
+
+$('#link-6sc4').click(function() {
+    $('#kostka').removeClass().addClass("turn6sc4");
+
+});
+
+$('#link-6sc5').click(function() {
+    $('#kostka').removeClass().addClass("turn6sc5");
+
+});
